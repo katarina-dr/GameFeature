4e1589dabfaefcfe979e8474a3440ff9f93360c8
katarina-dr <katarinadragicevic111@gmail.com>
2026-07-13 22:12:49 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/games/gamefeature/GameFeatureApplication.java
@@ -0,0 +1,13 @@
+package com.games.gamefeature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameFeatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameFeatureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/games/gamefeature/GameFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.gamefeature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameFeatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
